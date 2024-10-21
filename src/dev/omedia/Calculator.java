86f0bb09af7a69e3e29013760cc7cfff9f56824c
levanbn <levancho.begashvili@gmail.com>
2024-10-22 03:02:31 +0400

    add Section 7: OOP Part 1 - Inheritance exercise

---
+++ src/dev/omedia/Calculator.java
@@ -0,0 +1,15 @@
+package dev.omedia;
+
+public class Calculator {
+    Floor floor;
+    Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return this.floor.getArea() * this.carpet.getCost();
+    }
+}
